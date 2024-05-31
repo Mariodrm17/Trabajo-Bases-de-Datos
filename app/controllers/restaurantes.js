1f19cfa4191884a5938dc4aa5c58340a7ad63cba
RaulPlayo <raul.pelayo@alumnos.uneatlantico.es>
2024-05-31 14:54:17 +0200

    añadidoBuscarPorContinente

--- app/controllers/restaurantes.js
+++ app/controllers/restaurantes.js
@@ -2,18 +2,24 @@ const model = require('../models/restaurantes');
 
 exports.getData = async (req, res) => {
     try {
-        const aforo = parseInt(req.query.aforo, 10);
+        const { aforo, continente } = req.query;
 
-        if (!isNaN(aforo)) {
-            const restaurantes = await model.find({ capacidad: { $gte: aforo } });
-            if (restaurantes.length > 0) {
-                res.render('restaurantes', { restaurantes });
-            } else {
-                res.render('restaurantes', { error: 'No se encontraron restaurantes con el aforo especificado.' });
-            }
-        } else {
-            const restaurantes = await model.find();
+        let query = {};
+
+        if (aforo) {
+            query.capacidad = { $gte: parseInt(aforo, 10) };
+        }
+        
+        if (continente) {
+            query.continente = continente;
+        }
+
+        const restaurantes = await model.find(query);
+
+        if (restaurantes.length > 0) {
             res.render('restaurantes', { restaurantes });
+        } else {
+            res.render('restaurantes', { error: 'No se encontraron restaurantes que coincidan con los criterios de búsqueda.' });
         }
     } catch (error) {
         res.status(500).send('Hubo un error');
