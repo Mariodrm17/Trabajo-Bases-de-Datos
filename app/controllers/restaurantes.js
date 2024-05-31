9af6959c1c82302f841f3f8dacb58b67c453d421
RaulPlayo <raul.pelayo@alumnos.uneatlantico.es>
2024-05-31 14:42:33 +0200

    buscarXaforo

--- app/controllers/restaurantes.js
+++ app/controllers/restaurantes.js
@@ -2,14 +2,19 @@ const model = require('../models/restaurantes');
 
 exports.getData = async (req, res) => {
     try {
-        const aforo = req.query.aforo;
-        let restaurantes;
-        if (aforo) {
-            restaurantes = await model.find({ capacidad: { $gte: aforo } });
+        const aforo = parseInt(req.query.aforo, 10);
+
+        if (!isNaN(aforo)) {
+            const restaurantes = await model.find({ capacidad: { $gte: aforo } });
+            if (restaurantes.length > 0) {
+                res.render('restaurantes', { restaurantes });
+            } else {
+                res.render('restaurantes', { error: 'No se encontraron restaurantes con el aforo especificado.' });
+            }
         } else {
-            restaurantes = await model.find();
+            const restaurantes = await model.find();
+            res.render('restaurantes', { restaurantes });
         }
-        res.render('restaurantes', { restaurantes });
     } catch (error) {
         res.status(500).send('Hubo un error');
     }
