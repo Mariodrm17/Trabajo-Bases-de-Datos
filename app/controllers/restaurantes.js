d8e8b08a5c8d763a3e22a62ea71d4060f6e4e385
RaulPlayo <raul.pelayo@alumnos.uneatlantico.es>
2024-05-31 14:27:16 +0200

    commits

--- app/controllers/restaurantes.js
+++ app/controllers/restaurantes.js
@@ -2,9 +2,15 @@ const model = require('../models/restaurantes');
 
 exports.getData = async (req, res) => {
     try {
-        const restaurantes = await model.find();
+        const aforo = req.query.aforo;
+        let restaurantes;
+        if (aforo) {
+            restaurantes = await model.find({ capacidad: { $gte: aforo } });
+        } else {
+            restaurantes = await model.find();
+        }
         res.render('restaurantes', { restaurantes });
     } catch (error) {
         res.status(500).send('Hubo un error');
     }
-}
+};
